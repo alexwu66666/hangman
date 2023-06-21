b7ba4ed9be11970e310089a843bbf04f9bdeb28b
alexwu66666 <wuchao_bjfu@163.com>
2023-06-21 16:31:44 +0800

    add game

---
+++ game.py
@@ -0,0 +1,8 @@
+def game(p, t):
+    if p < 3:
+        if t > 5:
+            print("let's play chess!")
+        else:
+            print("let's play uno!")
+    else:
+        print("let's have a rest!")
